e4567804860bb33d5b9fe0faba5af58350349c87
Ramon <ramonsantospinto@gmail.com>
2022-04-12 11:16:32 -0300

    exercicios

---
+++ java/Estrutura de dados java/set/main.java
@@ -0,0 +1,40 @@
+package set;
+
+import com.sun.source.tree.Tree;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class main {
+
+    public static void main(String[] args){
+
+        Set<Carro> hashSetCarros = new HashSet<>();
+
+        hashSetCarros.add(new Carro("Ford"));
+        hashSetCarros.add(new Carro("Chevrolet"));
+        hashSetCarros.add(new Carro("Fiat"));
+        hashSetCarros.add(new Carro("Peugout"));
+        hashSetCarros.add(new Carro("Zip"));
+        hashSetCarros.add(new Carro("Alfa Romeo"));
+
+        System.out.println(hashSetCarros);
+
+        Set<Carro> treeSetCarros = new TreeSet<>();
+
+        treeSetCarros.add(new Carro("Ford"));
+        treeSetCarros.add(new Carro("Chevrolet"));
+        treeSetCarros.add(new Carro("Fiat"));
+        treeSetCarros.add(new Carro("Peugout"));
+        treeSetCarros.add(new Carro("Zip"));
+        treeSetCarros.add(new Carro("Alfa Romeo"));
+
+        System.out.println(treeSetCarros);
+
+    }
+
+
+
+
+}
